7d8a6e6e615e62015e36fad02b12ee0db5d1cca6
sebtex1 <36498890+sebtex1@users.noreply.github.com>
2023-01-12 14:56:29 +0100

    map & location

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
 import { View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Login from './pages/login';
-import Map from './pages/map';
+import Login from './pages/Login';
+import Map from './pages/Map';
 
-// const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Login}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen
-          name="Map"
-          component={Map}
-        />
-      </Stack.Navigator> */}
       <View style={{ paddingTop: 50 }}></View>
       
       <Tab.Navigator
--- pages/map.js
+++ pages/map.js
@@ -1,12 +1,50 @@
-import { View } from 'react-native';
-import { Text } from '@rneui/base';
+import { StyleSheet, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, Button } from '@rneui/base';
+import MapView, { Marker } from 'react-native-maps';
 
-const Map = ({navigation}) => {
+import * as Location from 'expo-location';
+
+  const Map = ({navigation}) => {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+        getLocation();
+      })();
+    }, []);
+
+    const getLocation = async () => {
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    }
+  
     return (
-      <View>
-        <Text>Map</Text>
+      <View style={styles.container}>
+        <MapView style={styles.map}>
+          <Marker coordinate={ {latitude: location?.coords?.latitude ?? 0, longitude: location?.coords?.longitude ?? 0} } />
+        </MapView>
       </View>
     );
   };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  });
 
 export default Map
