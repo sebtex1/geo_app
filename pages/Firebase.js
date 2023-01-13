c2f4145d6ae964687aa4a0cf091bad12b703293a
Olivier COUJANDASSAMY <olivier.coujan@gmail.com>
2023-01-13 11:08:44 +0100

    Auth listener and logout

--- pages/Firebase.js
+++ pages/Firebase.js
@@ -1,22 +1,51 @@
 import { Text, Button } from "@rneui/base";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { auth } from "../config/firebaseConfig";
-import { createUserWithEmailAndPassword } from "firebase/auth";
 import accountHelper from "../static/accountHelper";
 
 const Firebase = ({ navigation }) => {
+  const [userCred, setUserCred] = useState({});
+
+  //Listen to the user connection state
+  useEffect(() => {
+    console.log(userCred);
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log("Logged in");
+      } else {
+        console.log("Not logged in");
+      }
+    });
+
+    return unsubscribe;
+  }, [userCred]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Firebase</Text>
       <Button
-        title="test"
+        title="Signup"
         onPress={() =>
-          accountHelper.signupAccountWithEmail(
+          accountHelper.signupWithEmail(
             "olivier@gmail.com",
             "olivierbigboss123!"
           )
         }
+      />
+      <Button
+        title="Signin"
+        onPress={() =>
+          accountHelper.signinWithEmail(
+            "olivier@gmail.com",
+            "olivierbigboss123!",
+            setUserCred
+          )
+        }
+      />
+      <Button
+        title="Signout"
+        onPress={() => accountHelper.SignOut(setUserCred)}
       />
     </View>
   );
--- static/accountHelper.js
+++ static/accountHelper.js
@@ -1,11 +1,37 @@
 import { auth } from "../config/firebaseConfig";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 const accountHelper = {
-  signupAccountWithEmail: (email, password) => {
+  signupWithEmail: (email, password, setUserCred) => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((response) => {
+        setUserCred(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  signinWithEmail: (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((response) => {
         console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  SignOut: (setUserCred) => {
+    auth
+      .signOut()
+      .then((response) => {
+        console.log(response);
+        setUserCred({});
       })
       .catch((error) => {
         console.log(error);
