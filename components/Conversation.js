a9c3d4c944609c9b845ccc05ba8c142634b8392f
Olivier COUJANDASSAMY <olivier.coujan@gmail.com>
2023-01-20 09:59:11 +0100

    Add conversations tab

---
+++ components/Conversation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet } from "react-native";
+
+//Todo: use this component for the conversation list
+const Conversation = (props) => {
+    return <div></div>;
+};
+
+const styles = StyleSheet.create({});
+
+Conversation.propTypes = {};
+
+export default Conversation;
