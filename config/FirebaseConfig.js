3c8a15a00bc4e35f71a889895d0ce707516ce088
Olivier COUJANDASSAMY <olivier.coujan@gmail.com>
2023-01-26 15:54:44 +0100

    Add env variables

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -1,33 +1,36 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
-import { getDatabase } from "firebase/database";
+import {
+    FIREBASE_CONFIG_API_KEY,
+    FIREBASE_CONFIG_AUTH_DOMAIN,
+    FIREBASE_CONFIG_PROJECT_ID,
+    FIREBASE_CONFIG_STORAGE_BUCKET,
+    FIREBASE_CONFIG_MESSAGING_SEND_ID,
+    FIREBASE_CONFIG_APP_ID,
+} from "@env";
 
 const FirebaseConfig = {
-  apiKey: "AIzaSyC3c5UrTaZ5H3pb9bpgPdfzzzOX34EjYeo",
-  authDomain: "geo-app-ynov.firebaseapp.com",
-  projectId: "geo-app-ynov",
-  storageBucket: "geo-app-ynov.appspot.com",
-  messagingSenderId: "840342330616",
-  appId: "1:840342330616:web:403099b9f0add97bacef0d",
-  databaseURL: "https://geo-app-ynov-default-rtdb.europe-west1.firebasedatabase.app/"
+    apiKey: FIREBASE_CONFIG_API_KEY,
+    authDomain: FIREBASE_CONFIG_AUTH_DOMAIN,
+    projectId: FIREBASE_CONFIG_PROJECT_ID,
+    storageBucket: FIREBASE_CONFIG_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_CONFIG_MESSAGING_SEND_ID,
+    appId: FIREBASE_CONFIG_APP_ID,
 };
 
 let app;
 let auth;
 let database;
-let realTimeDB;
 
 // Initialize Firebase
 try {
-  app = initializeApp(FirebaseConfig);
-  auth = getAuth(app);
-  database = getFirestore();
-  realTimeDB = getDatabase(app);
+    app = initializeApp(FirebaseConfig);
+    auth = getAuth(app);
+    database = getFirestore();
 } catch (error) {
-  console.log("Error:", error);
+    console.log("Error:", error);
 }
 
 export { auth };
 export { database };
-export { realTimeDB };
