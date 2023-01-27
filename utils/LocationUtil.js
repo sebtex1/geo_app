7174b37e9e7b50f3be1ae1abeb930b83ade053b8
sebtex1 <36498890+sebtex1@users.noreply.github.com>
2023-01-27 11:55:02 +0100

    Fix: change var to const

--- utils/LocationUtil.js
+++ utils/LocationUtil.js
@@ -2,10 +2,10 @@ import * as Location from "expo-location";
 
 const LocationUtil = {
     distanceBetween: (location1, location2) => {
-        var R = 6371; // Radius of the earth in km
-        var dLat = (location1.latitude - location2.latitude) * (Math.PI / 180); // deg2rad below
-        var dLon = (location1.longitude - location2.longitude) * (Math.PI / 180);
-        var a =
+        const R = 6371; // Radius of the earth in km
+        const dLat = (location1.latitude - location2.latitude) * (Math.PI / 180); // deg2rad below
+        const dLon = (location1.longitude - location2.longitude) * (Math.PI / 180);
+        const a =
             0.5 -
             Math.cos(dLat) / 2 +
             (Math.cos((location1.latitude * Math.PI) / 180) * Math.cos((location2.latitude * Math.PI) / 180) * (1 - Math.cos(dLon))) / 2;
